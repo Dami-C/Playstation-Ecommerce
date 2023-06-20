c9b48699f2552751ad14ec268ab7858019542b7c
Damian Cordeiro <damian.cordeiro@live.com>
2023-06-19 22:52:02 -0300

    Agregando Firebase y Firestore al proyecto

--- src/components/ItemListContainer/ItemListContainer.jsx
+++ src/components/ItemListContainer/ItemListContainer.jsx
@@ -2,22 +2,50 @@ import { useEffect, useState } from "react";
 import { useParams } from "react-router-dom";
 import productos from "../asyncMock.json";
 import ItemList from "../ItemList/ItemList";
+import { getFirestore, collection, getDocs, where, query, addDoc } from "firebase/firestore"
 
 const ItemListContainer = () => {
     const [items, setItems] = useState([]);
     const {id} = useParams();
 
-    useEffect(() => {
-        const promesa = new Promise((resolve) => {
-            setTimeout(() => {
-                resolve(id ? productos.filter(item => item.categoria === id) : productos);
-            }, 1000);
-        });
+    //Proceso de importacion 
+    // useEffect(() => {
+    //     const db = getFirestore();
+    //     const itemsCollection = collection(db, "items");
 
-        promesa.then(data => {
-            setItems(data);
-        });
-    }, [id]);
+    //     productos.forEach(producto => {
+    //         addDoc(itemsCollection, producto);
+    //     });
+
+    //     console.log("Productos cargados desde Firestore!");
+
+    // },[]);
+
+    // useEffect(() => {
+    //     const promesa = new Promise((resolve) => {
+    //         setTimeout(() => {
+    //             resolve(id ? productos.filter(item => item.categoria === id) : productos);
+    //         }, 1000);
+    //     });
+
+    //     promesa.then(data => {
+    //         setItems(data);
+    //     });
+    // }, [id]);
+
+    // Acceder a una coleccion de documentos desde Firestore
+        useEffect(() => {
+            const db = getFirestore();
+            const itemsCollection = collection(db, "items");
+            const q = id ? query(itemsCollection, where("categoria", "==", id)) : itemsCollection;
+            getDocs(q).then(resultado => {
+                if (resultado.size > 0) {
+                    setItems(resultado.docs.map(producto => ({id:producto.id, ...producto.data()})));
+                } else {
+                    console.error("Error! No se encontraron productos en la colección!");
+                }
+            })
+        }, [id]);
 
     return (
         <div className="container my-5">
--- src/index.jsx
+++ src/index.jsx
@@ -2,6 +2,23 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import App from './App';
 import 'bootstrap/dist/css/bootstrap.min.css';
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+    apiKey: "AIzaSyDYbRAKNM9kKxr9E9RJAiKtAGmP9xXTUeU",
+    authDomain: "reactjsplaystationapp.firebaseapp.com",
+    projectId: "reactjsplaystationapp",
+    storageBucket: "reactjsplaystationapp.appspot.com",
+    messagingSenderId: "58859361216",
+    appId: "1:58859361216:web:9d3c6ea254c7309ce32e25"
+};
+
+// Initialize Firebase
+initializeApp(firebaseConfig);
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(<App />
@@ -10,3 +27,4 @@ root.render(<App />
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
 // or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
+
